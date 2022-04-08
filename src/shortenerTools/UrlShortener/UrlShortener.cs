a99dae4434c6d34c3706005fb4dafcc62c2093e1
Michael McKechney <michael@mckechney.com>
2022-04-08 15:27:45 -0400

    Added onenote link matching

--- src/shortenerTools/UrlShortener/UrlShortener.cs
+++ src/shortenerTools/UrlShortener/UrlShortener.cs
@@ -65,7 +65,7 @@ namespace Cloud5mins.Function
             }
 
             // Validates if input.url is a valid aboslute url, aka is a complete refrence to the resource, ex: http(s)://google.com
-            if (!input.Url.ToLower().StartsWith("http://") && !input.Url.ToLower().StartsWith("https://"))
+            if (!input.Url.ToLower().StartsWith("http://") && !input.Url.ToLower().StartsWith("https://") && !input.Url.ToLower().StartsWith("onenote:https://"))
             {
                 return new BadRequestObjectResult($"{input.Url} is not a valid absolute Url. The Url parameter must start with 'http://' or 'http://'.");
             }
